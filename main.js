3505dec3fdc712bcdfc2a52aa00ea424763478c6
juliafrois <166824761+juliafrois@users.noreply.github.com>
2024-06-14 09:47:22 -0300

    14/06/2024

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
-for(let i = 0; 1 < botoes.length ; i++){
+const conteudo = document.querySelectorAll(".aba-conteudo")
+const tempo = document.querySelectorAll(".contador")
+tempo[0].textContent = "ola"
+const tempoObjetivo1 = new Date(2024,4,16,19);
+const tempoObjetivo2 = new Date(05);
+const tempoObjetivo3 = new Date(2007);
+const tempoObjetivo4 = new Date(17);
+const tempoObjetivo5 = new Date(06);
+const agora = new Date();
+tempo[1].textContent = tempoObjetivo1
+console.log(botoes);
+for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function() {
         for (j=0;j<botoes.length;j++){
             botoes[j].classList.remove("ativo");
-            conteudo[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
