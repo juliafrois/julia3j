dd427372faee44f2c0eae0b43e372f81274a649a
juliafrois <166824761+juliafrois@users.noreply.github.com>
2024-06-20 07:52:36 -0300

    20/06/2024

--- main.js
+++ main.js
@@ -2,12 +2,21 @@ const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const tempo = document.querySelectorAll(".contador")
 tempo[0].textContent = "ola"
-const tempoObjetivo1 = new Date(2024,4,16,19);
+const tempoObjetivo1 = new Date(2007,4,16,17);
 const tempoObjetivo2 = new Date(05);
 const tempoObjetivo3 = new Date(2007);
 const tempoObjetivo4 = new Date(17);
 const tempoObjetivo5 = new Date(06);
 const agora = new Date();
+let segundos;
+let minutos;
+let horas;
+let dias;
+Segundos = (tempoObjetivo1 - agora)/1000
+Minutos = segundos/60
+Horas = minutos/60
+Dias = horas/24
+tempo[0].textContent = tempoObjetivo1 - agora
 tempo[1].textContent = tempoObjetivo1
 console.log(botoes);
 for(let i = 0; i < botoes.length ; i++){
